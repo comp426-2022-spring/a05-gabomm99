1d65fd5055902e9c7ff2b8f532bf85f6bee98cf8
Gabriel Mercado Mendez <gabomm99@ad.unc.edu>
2022-04-22 10:17:35 -0400

    Add some utilities and fixed export statement in config

--- src/config/general.config.js
+++ src/config/general.config.js
@@ -1,1 +1,19 @@
 // Configuration goes here
+const fs = require('fs')
+const morgan = require('morgan')
+//"Importing database script"
+const db = require("./database.js")
+//Requiring express for the whole app
+const express = require('express')
+const app = express()
+//App use() requirements
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//Export all of the above
+module.exports = {
+    fs: fs,
+    morgan: morgan,
+    db: db,
+    express: express,
+    app: app
+} 
